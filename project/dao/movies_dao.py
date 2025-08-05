99b5490db294304e07c392ae72471ed829b2018e
Georgiy Shvets <shvets_georgiy@mail.ru>
2025-08-05 18:03:52 +0300

    Dao files with GET queries is ready.

---
+++ project/dao/movies_dao.py
@@ -0,0 +1,13 @@
+from sqlalchemy import desc
+from project.dao.model.movie_model import Movie
+
+
+class MovieDao:
+    def __init__(self, session):
+        self.session = session
+
+    def get_all(self):
+        return self.session.query(Movie)
+
+    def get_one(self, mid):
+        return self.session.query(Movie).get(mid)
